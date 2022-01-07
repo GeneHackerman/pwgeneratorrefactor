de830e5dd1929db163ab16cb04681b13f1afe1db
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-01-07 15:43:16 -0500

    added window prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,58 @@
 // Assignment code here
+function generatePassword() {
+  var passwordLength = 0;
+  var upperCase;
+  var lowerCase;
+  var numeric;
+  var specialCharacter;
+
+  // add array for password length(?)
+
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = window.prompt("Please choose your length for password between 8 and 128 characters");
+    if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("Please enter a number between 8 and 128");
+
+    }
+  }
+  
+  while (upperCase !== "yes" && upperCase !== "no") {
+    upperCase = window.prompt("Would you like to include uppercase characters?");
+    upperCase.toLowerCase();
+    if (upperCase !== "yes" && upperCase !== "no")
+      window.alert("Please enter either 'yes' or 'no'.");
+  }
+
+  while (lowerCase !== "yes" && lowerCase !== "no") {
+    lowerCase = window.prompt("Would you like to include lowercase characters?");
+    lowerCase.toLowerCase();
+    if (lowerCase !== "yes" && lowerCase !== "no")
+      window.alert("Please FUCKING enter either 'yes' or 'no'.");
+  } 
+
+  while (numeric !== "yes" && numeric !== "no") {
+    numeric = window.prompt("Would you like to include a number?");
+    numeric.toLowerCase();
+    if (numeric !== "yes" && numeric !== "no")
+      window.alert("Please enter either 'yes' or 'no'.");
+  }
+
+  while (specialCharacter !== "yes" && specialCharacter !== "no") {
+    specialCharacter = window.prompt("Would you like to include special characters?");
+    specialCharacter.toLowerCase();
+    if (specialCharacter !== "yes" && specialCharacter !== "no")
+      window.alert("Please enter either 'yes' or 'no'.");
+  }
+
+
+  
+  console.log(passwordLength);
+  console.log(upperCase);
+  console.log(lowerCase);
+  console.log(numeric);
+  console.log(specialCharacter);
+  return upperCase + lowerCase;
+}
 
 
 // Get references to the #generate element
